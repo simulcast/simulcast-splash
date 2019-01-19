90776bd51a3e9136f988b1a7b548417acc6442af
simulcast <thisissimulcast@gmail.com>
2019-01-19 15:39:42 -0800

    fixed viewport height bullshit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     /* hide homepage */
     $("#content-main").hide();
     $("#content-bio").hide();
