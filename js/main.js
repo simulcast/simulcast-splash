8a197dbd21f57a96aeddfbacbaa45c432857570a
simulcast <thisissimulcast@gmail.com>
2019-02-11 13:29:40 -0800

    added pixelation by writing html5 vid to canvas

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     /* canvas */
-    const canvas = document.querySelector("canvas");
-    const ctx = canvas.getContext("2d");
-    const video = document.querySelector("bgvid");
-
-    video.addEventListener('play', () => {
-      function step() {
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
-        requestAnimationFrame(step)
-      }
-      requestAnimationFrame(step);
-    })
+    const video = document.querySelector('video')
+    const canvas = document.querySelector('canvas')
+    const ctx = canvas.getContext('2d')
+    video.addEventListener('play', start)
+    function start() {
+      // Set the canvas size to match the video dimensions
+      canvas.width = video.videoWidth
+      canvas.height = video.videoHeight
+      paintFrame()
+    }
+    function paintFrame() {
+      requestAnimationFrame(paintFrame)
+      // Every frame, paint the current video frame to the canvas
+      ctx.drawImage(video,0,0);
+      ctx.filter = "brightness(75%)";
+    }
     /* viewport shit */
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
